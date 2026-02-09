12855297d071faa5521b8c8a59f8f50a56014f9b
Solomon Ogu <solomonogu0@gmail.com>
2026-02-09 07:22:18 +0100

    fix: Update all locale settings to UTF-8

--- hosts/abuja/configuration.nix
+++ hosts/abuja/configuration.nix
@@ -27,18 +27,18 @@
   time.timeZone = "Africa/Lagos";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_NG";
+  i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_NG";
-    LC_IDENTIFICATION = "en_NG";
-    LC_MEASUREMENT = "en_NG";
-    LC_MONETARY = "en_NG";
-    LC_NAME = "en_NG";
-    LC_NUMERIC = "en_NG";
-    LC_PAPER = "en_NG";
-    LC_TELEPHONE = "en_NG";
-    LC_TIME = "en_NG";
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
   # Configure keymap in X11
